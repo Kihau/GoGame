53e4fd8138a12129e9b5bd2ca4a6e9d8f7d168ad
kihau <michal.haldas@gmail.com>
2022-01-19 21:14:44 +0100

    Update

--- include/Game.hpp
+++ include/Game.hpp
@@ -6,7 +6,11 @@
 class Game  
 {
 private:
+	const int width = 1280;
+	const int height = 720;
 	sf::RenderWindow* window;
+	sf::Sprite board;
+	sf::Texture tex;
 	float dt;
 
 private:
--- src/Game.cpp
+++ src/Game.cpp
@@ -1,11 +1,14 @@
-#include "Game.hpp"  
-	
+#include "Game.hpp"
+
 Game::Game()
 {
     this->dt = 0.0f;
-    this->window = new sf::RenderWindow(sf::VideoMode(1200, 800), "GoGame by Kihau");
+    this->window = new sf::RenderWindow(sf::VideoMode(width, height), "GoGame by Kihau");
+    this->tex.loadFromFile("resources/textures/go.png", sf::IntRect(0, 0, 1920, 1080));
+    this->board = sf::Sprite(tex);
+    this->board.scale(width / 1920.0f, height / 1080.0f);
 }
-	
+
 Game::~Game()
 {
 	delete this->window;
@@ -13,7 +16,21 @@ Game::~Game()
 
 void Game::run() {
     while(this->window->isOpen()) {
-        //this->update();
-        //this->render();
+        this->update();
+        this->render();
+    }
+}
+
+void Game::render() {
+    this->window->clear(sf::Color::Black);
+    this->window->draw(this->board);
+    this->window->display();
+}
+
+void Game::update() {
+    sf::Event event;
+    while (this->window->pollEvent(event)) {
+        if (event.type == sf::Event::Closed)
+            this->window->close();
     }
 }
